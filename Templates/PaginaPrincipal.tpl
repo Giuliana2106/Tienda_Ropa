76fbed55d6ff2b0f3df2930b0f5be3370ff242b0
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2022-05-18 22:43:29 -0600

    Nuevos Cambios

--- Templates/PaginaPrincipal.tpl
+++ Templates/PaginaPrincipal.tpl
@@ -13,11 +13,250 @@
     </div>
     
     <div class="row">
-    <div class="col s12 m6">
+    <div class="col s12 m4">
       <div class="card blue- amber lighten-4">
         <div class="card-content black-text">
         <div align="center">
-        <img class="materialboxed" width="300" src="images/vestido1.jpg">
+        <img class="materialboxed" width="150" src="images/vestido1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/vestido2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/vestido3.png">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/vestido4.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="93" src="images/vestido5.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="110" src="images/vestido6.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div align="center">
+    <h3>Blusas</h3>
+    </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/blusa1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/blusa2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/blusa3.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/blusa4.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/blusa5.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/blusa6.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div align="center">
+    <h3>Suéteres y Chaquetas</h3>
+    </div>
+    
+    <div class="row">
+    <div class="col s1 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/sueter1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/chaqueta1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s5 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/sueter2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/sueter3.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/chaqueta2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/sueter4.jpg">
         </div>
           <span class="card-title">Style Country</span>
           <p>I am a very simple card. I am good at containing small bits of information.
@@ -27,35 +266,6 @@
     </div>
   </div>
 
-  <div class="row">
-    <div class="col s10 m6">
-      <div class="card blue- amber lighten-4">
-        <div class="card-content black-text">
-        <div align="center">
-        <img class="materialboxed" width="300" src="images/vestido2.jpg">
-        </div>
-          <span class="card-title">Style Country</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col s10 m6">
-      <div class="card blue- amber lighten-4">
-        <div class="card-content black-text">
-        <div align="center">
-        <img class="materialboxed" width="300" src="images/vestido3.png">
-        </div>
-          <span class="card-title">Style Country</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-      </div>
-    </div>
-  </div>
 
 {include file="templates/Cabeceras/Footer.tpl"}
 
--- templates_c/3d776429c97de125a3db4f6ba9c84a9f708eeb61_0.file.PaginaPrincipal.tpl.php
+++ templates_c/3d776429c97de125a3db4f6ba9c84a9f708eeb61_0.file.PaginaPrincipal.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.0.4, created on 2022-04-19 05:07:16
+/* Smarty version 4.0.4, created on 2022-04-25 07:04:10
   from 'C:\xampp\htdocs\Tienda_Ropa\templates\PaginaPrincipal.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.0.4',
-  'unifunc' => 'content_625e27646b56c1_86279273',
+  'unifunc' => 'content_62662bca51e355_04862491',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '3d776429c97de125a3db4f6ba9c84a9f708eeb61' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Tienda_Ropa\\templates\\PaginaPrincipal.tpl',
-      1 => 1650337634,
+      1 => 1650862916,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/Cabeceras/Footer.tpl' => 1,
   ),
 ),false)) {
-function content_625e27646b56c1_86279273 (Smarty_Internal_Template $_smarty_tpl) {
+function content_62662bca51e355_04862491 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:templates/Cabeceras/Header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -39,11 +39,226 @@ $_smarty_tpl->_subTemplateRender("file:templates/Cabeceras/Header.tpl", $_smarty
     </div>
     
     <div class="row">
-    <div class="col s12 m6">
+    <div class="col s12 m4">
       <div class="card blue- amber lighten-4">
         <div class="card-content black-text">
         <div align="center">
-        <img class="materialboxed" width="300" src="images/vestido1.jpg">
+        <img class="materialboxed" width="150" src="images/vestido1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/vestido2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/vestido3.png">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/vestido4.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="93" src="images/vestido5.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="110" src="images/vestido6.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div align="center">
+    <h3>Blusas</h3>
+    </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/blusa1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/blusa2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/blusa3.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/blusa4.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/blusa5.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/blusa6.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div align="center">
+    <h3>Suéteres</h3>
+    </div>
+    
+    <div class="row">
+    <div class="col s1 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/sueter1.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s5 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="150" src="images/sueter2.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+    
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/sueter3.jpg">
+        </div>
+          <span class="card-title">Style Country</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col s12 m4">
+      <div class="card blue- amber lighten-4">
+        <div class="card-content black-text">
+        <div align="center">
+        <img class="materialboxed" width="100" src="images/sueter4.jpg">
         </div>
           <span class="card-title">Style Country</span>
           <p>I am a very simple card. I am good at containing small bits of information.
@@ -53,35 +268,6 @@ $_smarty_tpl->_subTemplateRender("file:templates/Cabeceras/Header.tpl", $_smarty
     </div>
   </div>
 
-  <div class="row">
-    <div class="col s10 m6">
-      <div class="card blue- amber lighten-4">
-        <div class="card-content black-text">
-        <div align="center">
-        <img class="materialboxed" width="300" src="images/vestido2.jpg">
-        </div>
-          <span class="card-title">Style Country</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col s10 m6">
-      <div class="card blue- amber lighten-4">
-        <div class="card-content black-text">
-        <div align="center">
-        <img class="materialboxed" width="300" src="images/vestido3.png">
-        </div>
-          <span class="card-title">Style Country</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-      </div>
-    </div>
-  </div>
 
 <?php $_smarty_tpl->_subTemplateRender("file:templates/Cabeceras/Footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
